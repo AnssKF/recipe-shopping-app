30c9cb833cb59c7308e8ae23c437ad5ce0c5c4e9
AnssKF <anss.khaled00@gmail.com>
2018-10-11 14:37:52 +0200

    Creating recipe service

---
+++ src/app/services/recipes/recipe.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, EventEmitter } from '@angular/core';
+
+import { Recipe } from '../../models/recipe.model';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RecipeService {
+
+  recipeSelected = new EventEmitter<Recipe>();;
+
+  private recipes: Recipe[] = [
+    new Recipe('recipeName', 'recipeDesc', 'https://images.media-allrecipes.com/images/56589.png'),
+    new Recipe('recipeName1', 'recipeDesc1', 'https://images.media-allrecipes.com/images/56589.png')
+  ]
+
+  constructor() { }
+
+
+  getRecipes(){
+    return this.recipes.slice();
+  }
+
+
+
+}
