5b71ebebf3662cfc16353073096009759154ad3b
AnssKF <anss.khaled00@gmail.com>
2018-10-11 15:54:58 +0200

    Create shoping list service

---
+++ src/app/services/shopping-list/shoppinglist.service.ts
@@ -0,0 +1,27 @@
+import { Injectable, EventEmitter } from '@angular/core';
+import { Ingredient } from '../../models/ingredient.model';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShoppinglistService {
+
+  newIngredient = new EventEmitter<Ingredient>();
+
+  private ingredients: Ingredient[] = [
+    new Ingredient('apples' , 30),
+    new Ingredient('tomatos', 60)
+  ];
+
+  constructor() { }
+
+  getIngredients(){
+    return this.ingredients;
+  }
+
+  addIngredient(ingredient:Ingredient){
+    this.ingredients.push(ingredient);
+  }
+
+}
