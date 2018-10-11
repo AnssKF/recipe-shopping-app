5798138a624a6b8fa9a54c7649a0a487dd98bc9f
AnssKF <anss.khaled00@gmail.com>
2018-10-11 15:55:24 +0200

    updating shoping list components with shopping list services

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output } from '@angular/core';
 import { Ingredient } from '../../models/ingredient.model';
+import { ShoppinglistService } from '../../services/shopping-list/shoppinglist.service';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -11,15 +12,15 @@ export class ShoppingEditComponent implements OnInit {
   @ViewChild('ingredientNameInput') ingredientNameRef: ElementRef;
   @ViewChild('ingredientAmountInput') ingredientAmountRef: ElementRef;
 
-  @Output('AddIngredient-Event') AddIngredient = new EventEmitter<Ingredient>();
 
-  onAddIngredient(){
-    this.AddIngredient.emit(new Ingredient(this.ingredientNameRef.nativeElement.value, this.ingredientAmountRef.nativeElement.value));
-  }
-
-  constructor() { }
+  constructor(private shoppinglistService:ShoppinglistService) { }
 
   ngOnInit() {
+  }
+
+  onAddIngredient(){
+    const ingredient = new Ingredient(this.ingredientNameRef.nativeElement.value , this.ingredientAmountRef.nativeElement.value);
+    this.shoppinglistService.newIngredient.emit(ingredient);
   }
 
 }
--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,26 +1,26 @@
 import { Component, OnInit } from '@angular/core';
 
 import { Ingredient } from '../models/ingredient.model';
+import { ShoppinglistService } from '../services/shopping-list/shoppinglist.service';
 
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
+  providers: [ShoppinglistService]
 })
 export class ShoppingListComponent implements OnInit {
 
-  ingredientsList: Ingredient[] = [
-    new Ingredient('apples' , 30),
-    new Ingredient('tomatos', 60)
-  ];
+  ingredientsList: Ingredient[];
 
-  onAddIngredient(ingredient:Ingredient){
-    this.ingredientsList.push(ingredient);
+  constructor(private shoppinglistService:ShoppinglistService) {
+    this.shoppinglistService.newIngredient.subscribe((ingredient:Ingredient)=>{
+      this.shoppinglistService.addIngredient(ingredient);
+    })
   }
 
-  constructor() { }
-
   ngOnInit() {
+    this.ingredientsList = this.shoppinglistService.getIngredients();
   }
 
 }
